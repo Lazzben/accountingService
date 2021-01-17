57485a92137d3d7c7746b200e5d0eb6df91e8d21
lazyben <458861463@qq.com>
2021-01-17 21:11:53 +0800

    test(converter): Implement UserInfoTest

--- src/test/java/com/lazyben/accounting/convert/c2s/UserInfoC2SConverterTest.java
+++ src/test/java/com/lazyben/accounting/convert/c2s/UserInfoC2SConverterTest.java
@@ -1,4 +1,60 @@
-package com.lazyben.accounting.convert;
+package com.lazyben.accounting.convert.c2s;
+
+import com.lazyben.accounting.model.common.UserInfo;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserInfoC2SConverterTest {
+
+    private final UserInfoC2SConverter userInfoC2SConverter = new UserInfoC2SConverter();
+
+    @Test
+    void testDoForward() {
+        // Arrange
+        val userId = 100L;
+        val username = "hardcore";
+        val password = "hardcore";
+
+        val userInfoInCommon = UserInfo.builder()
+                .id(userId)
+                .username(username)
+                .password(password)
+                .build();
+
+        // Act
+        val result = userInfoC2SConverter.convert(userInfoInCommon);
+
+        // Assert
+        assertThat(result).isNotNull()
+                .hasFieldOrPropertyWithValue("id", userId)
+                .hasFieldOrPropertyWithValue("username", username)
+                .hasFieldOrPropertyWithValue("password", null);
+    }
+
+    @Test
+    void testDoBackward() {
+        // Arrange
+        val userId = 100L;
+        val username = "hardcore";
+        val password = "hardcore";
+
+        val userInfoInCommon = com.lazyben.accounting.model.service.UserInfo.builder()
+                .id(userId)
+                .username(username)
+                .password(password)
+                .build();
+
+        // Act
+        val result = userInfoC2SConverter.reverse().convert(userInfoInCommon);
+
+        // Assert
+        assertThat(result).isNotNull()
+                .hasFieldOrPropertyWithValue("id", userId)
+                .hasFieldOrPropertyWithValue("username", username)
+                .hasFieldOrPropertyWithValue("password", password);
+
+    }
 }
+
--- src/test/java/com/lazyben/accounting/convert/p2c/UserInfoP2CConverterTest.java
+++ src/test/java/com/lazyben/accounting/convert/p2c/UserInfoP2CConverterTest.java
@@ -1,4 +1,64 @@
-package com.lazyben.accounting.convert;
+package com.lazyben.accounting.convert.p2c;
+
+import com.lazyben.accounting.model.persistence.UserInfo;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserInfoP2CConverterTest {
+
+    private final UserInfoP2CConverter userInfoP2CConverter = new UserInfoP2CConverter();
+
+    @Test
+    void testDoForward() {
+        // Arrange
+        val userId = 100L;
+        val username = "hardcore";
+        val password = "hardcore";
+        val createTime = LocalDate.now();
+
+        val userInfoInPersistence = UserInfo.builder()
+                .id(userId)
+                .username(username)
+                .password(password)
+                .createTime(createTime)
+                .build();
+
+        // Act
+        val result = userInfoP2CConverter.convert(userInfoInPersistence);
+
+        // Assert
+        assertThat(result).isNotNull()
+                .hasFieldOrPropertyWithValue("id", userId)
+                .hasFieldOrPropertyWithValue("username", username)
+                .hasFieldOrPropertyWithValue("password", password);
+    }
+
+    @Test
+    void testDoBackward() {
+        // Arrange
+        val userId = 100L;
+        val username = "hardcore";
+        val password = "hardcore";
+
+        val userInfoInCommon = com.lazyben.accounting.model.common.UserInfo.builder()
+                .id(userId)
+                .username(username)
+                .password(password)
+                .build();
+
+        // Act
+        val result = userInfoP2CConverter.reverse().convert(userInfoInCommon);
+
+        // Assert
+        assertThat(result).isNotNull()
+                .hasFieldOrPropertyWithValue("id", userId)
+                .hasFieldOrPropertyWithValue("username", username)
+                .hasFieldOrPropertyWithValue("createTime", null)
+                .hasFieldOrPropertyWithValue("password", password);
+    }
+
 }
